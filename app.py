cf9e9b4d42ee0f69a120e8cefa84715a8b4080f1
rushba <rushba@corp.badoo.com>
2021-11-26 06:06:09 +0300

    store and retrieve from mongodb

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import datetime
+
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
-app = Flask(__name__)
 
-entries = []
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://rushba:TjysuuJNpDCa444@udemymicro.j9e0z.mongodb.net/test")
+    app.db = client.microblog
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
 
-    entries_with_date = [
-        (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in entries
-    ]
-    return render_template("index.html", entries=entries_with_date)
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("index.html", entries=entries_with_date)
+
+    return app
